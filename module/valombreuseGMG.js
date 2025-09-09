34776ecd94c5b8956d64fe0e5d273cfae5d7e507
SEG91 <50146121+SEG91@users.noreply.github.com>
2025-09-09 16:25:44 +0200

    publish

---
+++ module/valombreuseGMG.js
@@ -0,0 +1,41 @@
+/**
+ * A simple and flexible system for world-building using an arbitrary collection of character and item attributes
+ * Author: Atropos
+ * Software License: GNU GPLv3
+ */
+
+
+
+
+
+
+Hooks.once("init", async function () {
+
+    console.info("----------   ValombreuseGMG : System Initializing...");
+
+    
+    console.info("ValombreuseGMG : Init Done");
+
+});
+
+Hooks.once("setup", function() {
+
+  console.info("ValombreuseGMG : Setup...");
+
+    
+
+  console.info("ValombreuseGMG : Setup done");
+
+  });
+
+
+  /* -------------------------------------------- */
+
+/**
+ * Once the entire VTT framework is initialized, check to see if we should perform a data migration
+ */
+Hooks.once("ready", function() {
+
+  console.info("ValombreuseGMG : Ready...");
+
+});
